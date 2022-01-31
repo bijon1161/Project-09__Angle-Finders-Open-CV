80250e4962f2cd093e685b8c2bca6f6487889a07
Bijon Mallik <bijon.ndc.pust@gmail.com>
2022-01-31 22:22:03 +0600

    On Left Button Mouse Click Shows Coordinates Values

--- AngleFinder.py
+++ AngleFinder.py
@@ -2,5 +2,11 @@ import cv2
 import math
 path = 'angles.jpg'
 img = cv2.imread(path)
+
+def mousePoints(event,x,y,flags,params):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        print(x,y)
+
 cv2.imshow('Image',img)
+cv2.setMouseCallback('Image',mousePoints)
 cv2.waitKey(0)
