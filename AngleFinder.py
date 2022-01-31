74953cf89f86fee0311e2efedffe495612516bcf
Bijon Mallik <bijon.ndc.pust@gmail.com>
2022-01-31 22:39:25 +0600

    Making Points Visible and Building Quitting Button To Refresh

--- AngleFinder.py
+++ AngleFinder.py
@@ -2,11 +2,16 @@ import cv2
 import math
 path = 'angles.jpg'
 img = cv2.imread(path)
+pointsList = []
 
 def mousePoints(event,x,y,flags,params):
     if event == cv2.EVENT_LBUTTONDOWN:
-        print(x,y)
-
-cv2.imshow('Image',img)
-cv2.setMouseCallback('Image',mousePoints)
-cv2.waitKey(0)
+        cv2.circle(img,(x,y),5,(0,0,255),cv2.FILLED)
+        pointsList.append([x,y])
+        
+        #print(x,y)
+        
+while True:
+    cv2.imshow('Image',img)
+    cv2.setMouseCallback('Image',mousePoints)
+    cv2.waitKey(1)
