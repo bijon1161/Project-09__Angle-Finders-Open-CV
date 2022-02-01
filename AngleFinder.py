40a88d3812b9897cf123153089da093a1c1a38f3
Bijon Mallik <bijon.ndc.pust@gmail.com>
2022-02-01 12:02:21 +0600

    Mathematical Solve and printing Angles

--- AngleFinder.py
+++ AngleFinder.py
@@ -10,8 +10,20 @@ def mousePoints(event,x,y,flags,params):
         pointsList.append([x,y])
         
         #print(x,y)
-        
+ 
+def gradient(pt1,pt2):
+    return (pt2[1]-pt1[1])/(pt2[0]-pt1[0])
+
+def getAngle(pointsList):
+    pt1,pt2,pt3 = pointsList[-3:]
+    m1 = gradient(pt1, pt2)
+    m2 = gradient(pt1, pt3)
+    angR = math.atan((m2-m1)/(1+(m1*m2))) 
+    angD = round(math.degrees(angR))
+    print(angD)
 while True:
+    if len(pointsList) % 3 ==0 and len(pointsList)!=0:
+        getAngle(pointsList)
     cv2.imshow('Image',img)
     cv2.setMouseCallback('Image',mousePoints)
     if cv2.waitKey(1) & 0xFF == ord('q'):
