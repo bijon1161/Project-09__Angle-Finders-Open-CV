a20e8dcf617b194aeb3feda9135ff1d9cbee0a10
Bijon Mallik <bijon.ndc.pust@gmail.com>
2022-02-01 17:29:17 +0600

    Final Part: Line Connecting and Showing angle on screen

--- AngleFinder.py
+++ AngleFinder.py
@@ -6,6 +6,9 @@ pointsList = []
 
 def mousePoints(event,x,y,flags,params):
     if event == cv2.EVENT_LBUTTONDOWN:
+        size = len(pointsList)
+        if size !=0 and size%3 !=0:
+            cv2.line(img,tuple(pointsList[round((size-1)/3)*3]),(x,y),(0,0,255),2)
         cv2.circle(img,(x,y),5,(0,0,255),cv2.FILLED)
         pointsList.append([x,y])
         
@@ -19,8 +22,8 @@ def getAngle(pointsList):
     m1 = gradient(pt1, pt2)
     m2 = gradient(pt1, pt3)
     angR = math.atan((m2-m1)/(1+(m1*m2))) 
-    angD = round(math.degrees(angR))
-    print(angD)
+    angD = abs(round(math.degrees(angR)))
+    cv2.putText(img,str(angD),(pt1[0]-40,pt1[1]-20),cv2.FONT_HERSHEY_COMPLEX_SMALL,1.5,(0,0,255),2)
 while True:
     if len(pointsList) % 3 ==0 and len(pointsList)!=0:
         getAngle(pointsList)
